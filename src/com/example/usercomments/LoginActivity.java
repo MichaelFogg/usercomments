47a9932bc49932a4e0fa2cadf29881a4c1bae829
MichaelFogg <mkf@zoomtown.com>
2013-10-22 16:39:56 -0400

    its up?

---
+++ src/com/example/usercomments/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.usercomments;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+	
+	/** Called when the user clicks the Send button */
+	public void performLogin(View view) {
+	    // Do something in response to button
+		
+		Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://dev.m.gatech.edu/login?url=usercomments://loggedin&sessionTransfer=window"));
+		startActivity(myIntent);
+	}
+}
